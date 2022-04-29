fb31eacacb39b71bf3180de0efe58a3edb8ab9f9
Amir Zec <amirzecdev@outlook.com>
2022-04-29 18:42:43 +0200

    migration created

--- database/migrations/2022_04_29_163940_create_authors_table.php
+++ database/migrations/2022_04_29_163940_create_authors_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
         Schema::create('authors', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->string('country');
+            $table->string('genre');
         });
     }
 
